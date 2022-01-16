ddd7385cbd93f4321b86baa100f22972623684c8
Alicja Lewandowska <ala.lew97@gmail.com>
2022-01-16 21:49:40 +0100

    teachers footer

--- single-teachers.php
+++ single-teachers.php
@@ -34,36 +34,45 @@ get_header(); ?>
         <div class="row justify-content-center">
             <div class="col-6">
                 <div class="row justify-content-center">
-                <div class="col-12">
-                    <img src="<?php echo get_field("teacher_img") ?>">
-                </div>
-                <div class="col-xxl-8 col-xl-6 col-sm-12 mb-4">
-                    <div><h3><?php echo get_field('teacher_name'); ?></h3></div>
-                    <div><p><?php echo get_field("teacher_email") ?></p></div>
-                </div>
+                    <div class="col-12">
+                        <img src="<?php echo get_field("teacher_img") ?>">
+                    </div>
+                    <div class="col-xxl-8 col-xl-6 col-sm-12 mb-4">
+                        <div>
+                            <h3><?php echo get_field('teacher_name'); ?></h3>
+                        </div>
+                        <div>
+                            <p><?php echo get_field("teacher_email") ?></p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-6 lectors-courses">
-                
-                <?php if ($oferta_query->have_posts()) : ?>
-                    <?php while ($oferta_query->have_posts()) : ?>
-                        <?php $oferta_query->the_post(); ?>
-                            <?php if ($teacher_name === get_field('lector_name') && (get_field('chose_course_type') === 'Online' || is_user_logged_in())) : ?>
 
-                                <div class="row justify-content-center">
-                                    <div class="col-xxl-4 col-xl-6 col-sm-12 mb-4">
-                                        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="flag">
-                                    </div>
-                                    <div class="col-xxl-8 col-xl-6 col-sm-12 mb-4">
-                                        <h3> <?php echo get_the_title(); ?> </h3>
-                                        <p> Poziom: <?php echo get_field("courses_level", get_the_ID()) ?> </p>
-                                        <a class='btn-gold-primary d-inline-block' href="<?php echo get_the_permalink()?>">Czytaj więcej <i class="fas fa-chevron-right"></i></a>
-                                    </div>
-                                </div>
-                            <?php endif; ?>
-                    <?php endwhile; ?> 
+                <?php if ($oferta_query->have_posts()) : ?>
+                <?php while ($oferta_query->have_posts()) : ?>
+                <?php $oferta_query->the_post(); ?>
+                <?php if ($teacher_name === get_field('lector_name') && (get_field('chose_course_type') === 'Online' || is_user_logged_in())) : ?>
+
+                <div class="row justify-content-center">
+                    <div class="col-xxl-4 col-xl-6 col-sm-12 mb-4">
+                        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'medium'); ?>" alt="flag">
+                    </div>
+                    <div class="col-xxl-8 col-xl-6 col-sm-12 mb-4">
+                        <h3> <?php echo get_the_title(); ?> </h3>
+                        <p> Poziom: <?php echo get_field("courses_level", get_the_ID()) ?> </p>
+                        <a class='btn-gold-primary d-inline-block' href="<?php echo get_the_permalink()?>">Czytaj więcej
+                            <i class="fas fa-chevron-right"></i></a>
+                    </div>
+                </div>
+                <?php endif; ?>
+                <?php endwhile; ?>
                 <?php endif;?>
             </div>
         </div>
     </div>
 </section>
+
+<?php
+
+get_footer(); ?>
