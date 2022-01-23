adbe883b55d9b92b17088597d878f7f33bffdedc
toronir <36642076+toronir@users.noreply.github.com>
2022-01-23 20:59:00 +0100

    New email body

--- inc/api.php
+++ inc/api.php
@@ -18,117 +18,72 @@ add_action('rest_api_init', function () {
 
 
             $to = 'toronir5@gmail.com';
-            $subject = 'Hey! Masz nowego kandydata';
-            $body = `<html >
-            <head>
-            <title>HTML CSS JS</title>
-              <link href="https://fonts.googleapis.com/css2?family=Saira+Semi+Condensed:wght@200;400;600&display=swap"
-                    rel="stylesheet">
-            </head>
-              
-               <style type="text/css">
-               
-                 table{
-                       border-spacing: 0px;
-                 }
+            $subject1 = 'Hey! Masz nowego kandydata';
+            $subject2 = 'Zostało bardzo niewiele!';
+            $body = `<body >
+
+            <div style="  margin: auto;
+                 width: 600px;
+                 background-color: white;">
+
+                  <table  width="100%" style=" border-bottom: solid #c29f48; border-spacing: 0px;">
+                     <tr>
                   
-                  h2 {
-                    font-size: 56px;
-                    font-family: 'Saira Semi Condensed', sans-serif;
-                  }
-                  
-                    h3{
-                    font-size: 28px;
-                    font-weight: 900; 
-                        font-family: 'Saira Semi Condensed', sans-serif;
-                  }
-                  
-                  p {
-                      font-family: 'Saira Semi Condensed', sans-serif;
-                    font-weight: 100;
-                  }
-                  
-                  td {
-                vertical-align: top;
-                  }
-                  
-                  #email {
-                    margin: auto;
-                    width: 600px;
-                    background-color: white;
-                  }
-                  
-                  a{
-                   
-                    text-decoration: none;
-                    font: inherit;
-                    background-color: #c29f48;
-                    border: none;
-                    padding: 10px;
-                    text-transform: uppercase;
-                    letter-spacing: 2px;
-                    font-weight: 900; 
-                    color: white;
-                    border-radius: 5px; 
-                    box-shadow: 3px 3px #808080;
-                  }
-                  
-                  .subtle-link {
-                    font-size: 9px; 
-                    text-transform:uppercase; 
-                    letter-spacing: 1px;
-                    color: #CBD6E2;
-                  }
-                  
-                </style>
-             <body >
-               
-               <div id="email">
-            
-              
-              
-              <! Banner --> 
-                     <table  width="100%" style=" border-bottom: solid #c29f48;  ;
-            
-             
-              ">
-                        <tr>
+                       <td bgcolor="#212529" align="center" style="color: white;">
                      
-                          <td bgcolor="#212529" align="center" style="color: white;">
+                      <img alt="Flower" src="$logo" width="400px" align="middle">
+                         
+                         <h2 style='color: #c29f48;font-size: 56px;'>  Nowy użytkownik czeka na akceptację </h2>
+                         
+                       </td>
+                 </table>
+            
+            
+         <table bgcolor="#212529" role="presentation" border="0" cellpadding="0" cellspacing="10px" style="border-spacing: 0px; padding: 30px 30px 30px 60px;color: #c29f48;">
+              <tr>
+                <td>
+                 <h3 align="center" style="  font-size: 28px;
+                 font-weight: 900;"> Nowy kandydat $firstName $secondName wysyła swoją prośbę!</h3>
+                     <p align="center" style="margin-bottom: 2rem">
+                       Aby dowiedzieć się więcej o nowym kandydacie przejdź do panelu administracyjnego strony CatArmy lub kliknij w poniższy przycisk  
+                     </p>
+            <div style="display:flex;justify-content:center;">
+                         <a style="text-decoration: none;
+                 font: inherit;
+                 background-color: #c29f48;
+                 border: none;
+                 padding: 10px;
+                 text-transform: uppercase;
+                 letter-spacing: 2px;
+                 font-weight: 900; 
+                 color: white;
+                 border-radius: 5px; 
+                 box-shadow: 3px 3px #808080;" href="http://front3.work-on.pl/wp-admin/">Wyświetl nowego użytkownika</a>
+                  </div>
                         
-                         <img alt="Flower" src="$logo" width="400px" align="middle">
-                            
-                            <h2 style='color: #c29f48;'>  Nowy użytkownik czeka na akceptację </h2>
-                            
-                          </td>
-                    </table>
-               
-               
-            <table bgcolor="#212529" role="presentation" border="0" cellpadding="0" cellspacing="10px" style="padding: 30px 30px 30px 60px;color: #c29f48;">
-                 <tr>
-                   <td>
-                    <h3 align="center"> Nowy kandydat $firstName $secondName wysyła swoją prośbę!</h3>
-                        <p align="center" style="margin-bottom: 2rem">
-                          Aby dowiedzieć się więcej o nowym kandydacie przejdź do panelu administracyjnego strony CatArmy lub kliknij w poniższy przycisk  
-                        </p>
-               <div style="display:flex;justify-content:center;">
-                            <a style="font-family: 'Saira Semi Condensed', sans-serif;" href="http://front3.work-on.pl/wp-admin/">Wyświetl nowego użytkownika</a>
-                     </div>
-                           
-                            
-                      </td> 
-                      </tr>
-                             </table>
-                 <table role="presentation" style="background-color: #c29f48" width="100%" >
-                  <tr>
-                      <td align="left" style="padding: 30px 30px;">
-                        <p style="color:#212529"> Z miłością &hearts; od CatTeam </p>
-                          <a  class="subtle-link" style="font-family: 'Saira Semi Condensed', sans-serif; background-color:#212529;" href="http://front3.work-on.pl/"> Do strony </a>      
-                      </td>
-                      </tr>
-                  </table> 
-                        </body>
-            </html>`;
+                         
+                   </td> 
+                   </tr>
+                          </table>
+              <table role="presentation" style="background-color:#c29f48; border-spacing: 0px;" width="100%" >
+               <tr>
+                   <td align="left" style="padding: 30px 30px;">
+                     <p style="color:#212529; font-weight: 100;"> Z miłością &hearts; od CatTeam </p>
+                       <a  class="subtle-link" style="text-decoration: none;
+                 font: inherit;
+                 background-color: #c29f48;
+                 border: none;
+                 padding: 10px;
+                 text-transform: uppercase;
+                 letter-spacing: 2px;
+                 font-weight: 900; 
+                 color: white;
+                 border-radius: 5px; 
+                 box-shadow: 3px 3px #808080; background-color:#212529;" href="http://front3.work-on.pl/"> Do strony </a>      
+                   </td>
+                   </tr>
+               </table> 
+                </body>`;
 
 
             $userList = get_users('blog_id=0&orderby=nicename');
@@ -158,7 +113,8 @@ add_action('rest_api_init', function () {
                 $output = 'success';
                 add_filter('wp_mail_content_type', 'set_html_content_type');
 
-                wp_mail($to, $subject, $body);
+                wp_mail($to, $subject1, $body);
+                wp_mail($email, $subject2, $body);
                 remove_filter('wp_mail_content_type', 'set_html_content_type');
             } else {
 
